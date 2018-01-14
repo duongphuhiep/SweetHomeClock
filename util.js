9ba0148211aa484423bb9b58628fd7737a27cb87
Phu-Hiep DUONG <duongphuhiep@gmail.com>
2018-01-15 00:29:34 +0100

    complete the basic

--- util.js
+++ util.js
@@ -40,115 +40,15 @@ var Tools = {
 
     dateTimeToString(d) {
       return dateToString(d)+' '+timeToString(d);
+    },
+
+    //convert date to "10 minute ago"
+    dateTimeToSinceString(d, now) {
+        if (typeof d === "undefined" || d===null)
+            return "-";
+        let sec = Math.floor((now.getTime()-d.getTime())/1000);
+        if (sec > 60)
+            return Math.round(sec/60)+" min ago";
+        return sec+" sec ago";
     }
 };
-/*
-
-class MissionNextUpdater {
-
-    constructor(lastData, lastError) {
-        this.lastData = null;
-        this.lastError = null;
-        this.nextUpdateInSec = 0;
-        this.nextUpdate = new Date();
-        this.lastUpdate = new Date();
-        this.isUpdating = false
-    }
-
-    update() {
-        if (this.isUpdating = true) {
-
-        }
-        this.lastUpdate = new Date();
-        let $this = this;
-        axios.get('./api/getMissionsNext')
-            .then(function (response) {
-                $this.lastError = null;
-                let data = response.data.return;
-
-                //check if response is validate
-
-                if (typeof data=='undefined' || typeof data.missions=='undefined') {
-                    $this.lastError = {message: "Invalid response", response};
-                    $this.lastData = null;
-                    let nextUpdateInSec = 150 //2.5min
-                    $this.nextUpdateInSec = nextUpdateInSec; //2.5min
-                    return;
-                }
-
-
-                let missions = data.missions;
-                if (missions==null || !(missions && missions.length)){
-                    $this.lastError = {message: "Empty next mission", response};
-                    $this.lastData = null;
-                    let nextUpdateInSec = 150 //2.5min
-                    $this.nextUpdateInSec = nextUpdateInSec; //2.5min
-                    return;
-                }
-
-                //compute the next update
-                let now = new Date();
-
-                //find the nearest next (future) mission
-                let nearestDateInFuture = new Date(Math.min.apply(null, missions.map(function(m) {
-                    let missionDateTimeStr = m.stationsDates[0];
-                    if (typeof missionDateTimeStr=='undefined' || missionDateTimeStr==null) {
-                        return MAXDATE;
-                    }
-                    let missionDateTime =  Tools.toDate(missionDateTimeStr)
-                    if (missionDateTime < now) return MAXDATE;
-                    return missionDateTime;
-                })));
-
-
-                if (nearestDateInFuture==MAXDATE) {
-                    $this.lastError = {message: "Unable to find nearest future mission (now = "+now.toString()+")", response};
-                    $this.lastData = null;
-                    let nextUpdateInSec = 300 //2.5min
-                    $this.nextUpdateInSec = nextUpdateInSec; //2.5min
-                    return;
-                }
-
-                let timeToWaitInSecond = Math.floor((nearestDateInFuture.getTime() - now.getTime())/3600);
-                if (timeToWaitInSecond<0) {
-                    $this.lastError = {message: "unexpected situation nearestDateInFuture="+nearestDateInFuture.toString()+"; now="+now.toString(), response};
-                    $this.lastData = null;
-                    let nextUpdateInSec = 300 //5min
-                    $this.nextUpdateInSec = nextUpdateInSec; //2.5min
-                    return;
-                }
-
-                console.log("timeToWaitInSecond", timeToWaitInSecond, nearestDateInFuture, now);
-
-                let nextUpdateInSec = timeToWaitInSecond / 2;
-
-                if (timeToWaitInSecond < 3) {
-                    $this.nextUpdateInSec = 3; //update after 3s at most
-                }
-
-                if (timeToWaitInSecond>1800) {
-                    $this.nextUpdateInSec = 900; //update after 15min at least
-                }
-
-                $this.lastData = data;
-                $this.nextUpdateInSec = nextUpdateInSec;
-            })
-            .catch(function (error) {
-                $this.lastError = error;
-                $this.lastData = null;
-                $this.nextUpdateInSec = 300; //5min
-            })
-            .finally(()=>{
-                //console.info("update finised ", $this.nextUpdateInSec, $this.lastData, $this.lastError);
-                if (3>$this.nextUpdateInSec || $this.nextUpdateInSec>900) {
-                    $this.lastError = {message: "unexpected situation"};
-                    $this.nextUpdateInSec = 150;
-                }
-
-                console.log("next update in ",$this.nextUpdateInSec)
-                setTimeout($this.update, $this.nextUpdateInSec*1000);
-
-                EventBus.$emit("MISSION_UPDATE", $this);
-            });
-    }
-}*/
